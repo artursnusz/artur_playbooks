243a38be44217843edfef660fbd270a0685a364c
artursnusz <as.artur@gmail.com>
2022-03-23 13:40:20 +0100

    Update test2.groovy

--- test/test2.groovy
+++ test/test2.groovy
@@ -1,8 +1,9 @@
 import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
 
-def SERVER_ENV = ["test", "labo", "development", "acceptance", "quality", "production"];
+def ROLE_NAME = "dmk-hpc"
+def SERVER_ENV = ["test", "development", "acceptance", "production"];
 
-node {
+node("master") {
     properties([
         disableConcurrentBuilds(),
         parameters([
@@ -14,13 +15,15 @@ node {
                     "{\"key\": \"${SERVER_ENV[0]}\",\"value\": \"${SERVER_ENV[0]}\"}," +
                     "{\"key\": \"${SERVER_ENV[1]}\",\"value\": \"${SERVER_ENV[1]}\"}," +
                     "{\"key\": \"${SERVER_ENV[2]}\",\"value\": \"${SERVER_ENV[2]}\"}," +
-		    "{\"key\": \"${SERVER_ENV[3]}\",\"value\": \"${SERVER_ENV[3]}\"}," +
-		    "{\"key\": \"${SERVER_ENV[4]}\",\"value\": \"${SERVER_ENV[4]}\"}," +
-                    "{\"key\": \"${SERVER_ENV[5]}\",\"value\": \"${SERVER_ENV[5]}\"}" +
+                    "{\"key\": \"${SERVER_ENV[3]}\",\"value\": \"${SERVER_ENV[3]}\"}" +
                 "]}", 
                 description: 'Select for which environment pipeline should run'
             )
         ]),
+        pipelineTriggers([
+            // schedule execution every 30 minutes, required plugin "Parameterized Scheduler"
+            parameterizedCron('H/30 * * * * %Server Environment=' + SERVER_ENV.join(","))
+        ])
     ])
     // requires plugin "AnsiColor"
     ansiColor('xterm') {
@@ -28,7 +31,7 @@ node {
             
             stage("${SERVER_ENV[i]}") {
                 if (params['Server Environment'].indexOf("${SERVER_ENV[i]}") >= 0) {
-                    echo "All works fantastic";
+                    echo "Test";
                 } else {
                     echo "Nothing to do in the stage - stage not selected to run";
                     Utils.markStageSkippedForConditional("${SERVER_ENV[i]}")
