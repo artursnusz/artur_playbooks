a158df5efe29641b8a17f399cc5cd2a10ead142b
artursnusz <as.artur@gmail.com>
2022-03-28 19:26:46 +0200

    Update test.groovy

--- test/test.groovy
+++ test/test.groovy
@@ -34,7 +34,7 @@ node {
         try{
             stage("${SERVER_ENV[i]}") {
                 if (params['Server Environment'].indexOf("${SERVER_ENV[i]}") >= 0) {
-                    if(SERVER_ENV[i] == "test" || SERVER_ENV[i] == "test2" || ){
+                    if(SERVER_ENV[i] == "test" || SERVER_ENV[i] == "test2" ){
                         echo SERVER_ENV[i]
                         sh 'exit 1';
                     }
